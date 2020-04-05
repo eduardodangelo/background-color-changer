caac5e0f8ce11c9ed6e5f67b58d5801327bf51ed
Eduardo D'Angelo <html5news@gmail.com>
2020-04-05 18:13:16 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('changeColor');
+const body = document.querySelector('body');
+
+btn.addEventListener('click', () => { 
+  body.classList.toggle('alt-color');
+});
