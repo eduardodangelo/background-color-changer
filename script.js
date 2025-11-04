3016dfaa24711da60c236c83915f17d16b973af4
Eduardo D'Angelo <html5news@gmail.com>
2025-11-04 13:25:11 -0500

    add

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const btn = document.getElementById('changeColor');
-const body = document.querySelector('body');
+// const btn = document.getElementById('changeColor');
+// const body = document.querySelector('body');
 
-btn.addEventListener('click', () => { 
-  body.classList.toggle('alt-color');
-});
+// btn.addEventListener('click', () => { 
+//   body.classList.toggle('alt-color');
+// });
 
+console.log(4);
+let hola = 'perus';
+document.write(hola);
 
